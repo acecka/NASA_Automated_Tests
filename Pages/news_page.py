2c5abdf2160e06cf84a51fbf3bfc55c5e9c5aeb0
acecka <agn.cecka@gmail.com>
2025-05-07 21:34:46 +0200

    news page tests cleaned and updated with new content

--- Pages/news_page.py
+++ Pages/news_page.py
@@ -3,13 +3,20 @@ from selenium.webdriver.common.by import By
 
 
 class NewsPageLocators:
-    BLOG1_BLOCK = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[1]/a')
-    BLOG1_TITLE = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[1]/a/div/div/div[2]/p/span')
-    BLOG2_BLOCK = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/a')
-    BLOG3_BLOCK = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[1]')
-    BLOG4_BLOCK = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[2]')
+    NEWS1_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[1]/a')
+    NEWS1_PANEL_TITLE = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[1]/a/div/div/div[2]/p/span')
+    NEWS2_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/a')
+    NEWS2_PANEL_TITLE = (By.XPATH, '//*[@id="post-128945"]/div/div[3]/div/div[2]/div[1]/div[2]/a/div/div/div[2]/p/span')
+    NEWS3_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[1]')
+    NEWS4_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[2]')
+    ARTICLE_TITLE = (By.XPATH, '/html/body/main/article/section/div[1]/div[2]/div/div/div/h1')
 
 
 class NewsPage(BasePage):
     def click_blog1(self):
-        self.driver.find_element(*NewsPageLocators.BLOG1_BLOCK).click()
+        self.driver.find_element(*NewsPageLocators.NEWS1_PANEL).click()
+
+    def get_article_title(self):
+        return self.driver.find_element(*NewsPageLocators.ARTICLE_TITLE).text
+
+
--- Tests/news_test.py
+++ Tests/news_test.py
@@ -2,6 +2,7 @@ from Tests.base_test import BaseTest
 from Pages.news_page import NewsPageLocators
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 
 class NewsPageTest(BaseTest):
@@ -10,19 +11,30 @@ class NewsPageTest(BaseTest):
         self.news_page = self.home_page.expand_news()
         self.news_page = self.home_page.click_news()
 
-    def testGoToBlog1(self):
+    def testVerifyNewsPanel1(self):
         """
         Checks Blog 1 navigation menu accuracy
         """
-        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(NewsPageLocators.BLOG1_BLOCK))
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(NewsPageLocators.NEWS1_PANEL))
 
-        block1_url = self.driver.find_element(*NewsPageLocators.BLOG1_BLOCK)
-        block1_link = block1_url.get_attribute("href")
-        self.assertIsNotNone(block1_link, "Missing href attribute")
+        news1_url = self.driver.find_element(*NewsPageLocators.NEWS1_PANEL)
+        news1_link = news1_url.get_attribute("href")
+        self.assertIsNotNone(news1_link, "Missing href attribute")
+        news_title = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(NewsPageLocators.NEWS1_PANEL_TITLE)).text
         # navigate to the URL
         self.news_page.click_blog1()
         # ensure actual navigation is correct
-        block_navigation = self.driver.current_url
-        self.assertEqual(block_navigation, block1_link, "URL mismatch")
+        panel_navigation = self.driver.current_url
+        self.assertEqual(panel_navigation, news1_link, "URL mismatch")
+        # ensure article title is correct
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(NewsPageLocators.ARTICLE_TITLE))
+        article_title = self.news_page.get_article_title()
+        # article_title = WebDriverWait(self.driver, 10).until(
+        #     EC.visibility_of_element_located(NewsPageLocators.ARTICLE_TITLE)).text
+        self.assertEqual(news_title, article_title, "Title mismatch")
+        sleep(2)
 
 
