365fb339fb2e4839a89b2b1c69a98c7b9367b167
acecka <agn.cecka@gmail.com>
2025-05-08 15:48:36 +0200

    added tests for 2nd news panel + updated news_page methods for future tests

--- Pages/news_page.py
+++ Pages/news_page.py
@@ -10,19 +10,54 @@ class NewsPageLocators:
     NEWS2_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/a')
     NEWS2_PANEL_TITLE = (By.XPATH, '//*[@id="post-128945"]/div/div[3]/div/div[2]/div[1]/div[2]/a/div/div/div[2]/p/span')
     NEWS3_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[1]')
+    NEWS3_PANEL_TITLE = (By.XPATH, '//*[@id="post-128945"]/div/div[3]/div/div[2]/div[1]/div[2]/div/a[1]/div/div/div[2]/p/span')
     NEWS4_PANEL = (By.XPATH, '/html/body/main/article/div/div[3]/div/div[2]/div[1]/div[2]/div/a[2]')
+    NEWS4_PANEL_TITLE = (By.XPATH, '//*[@id="post-128945"]/div/div[3]/div/div[2]/div[1]/div[2]/div/a[2]/div/div/div[2]/p/span')
     ARTICLE_TITLE = (By.XPATH, '/html/body/main/article/section/div[1]/div[2]/div/div/div/h1')
+
+    ARTICLE_PANEL = (By.XPATH, '//*[@id="post-860982"]/section/div[2]/div[2]/div/div/div/div/figure/a/img')
 
 
 class NewsPage(BasePage):
+
+    def get_article_img(self):
+        self.driver.find_element(*NewsPageLocators.ARTICLE_PANEL).get_attribute("img")
+
     def click_news1(self):
         self.driver.find_element(*NewsPageLocators.NEWS1_PANEL).click()
+
+    def click_news2(self):
+        self.driver.find_element(*NewsPageLocators.NEWS2_PANEL).click()
+
+    def click_news3(self):
+        self.driver.find_element(*NewsPageLocators.NEWS3_PANEL).click()
+
+    def click_news4(self):
+        self.driver.find_element(*NewsPageLocators.NEWS4_PANEL).click()
 
     def get_news1_href(self):
         return self.driver.find_element(*NewsPageLocators.NEWS1_PANEL).get_attribute("href")
 
+    def get_news2_href(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS2_PANEL).get_attribute("href")
+
+    def get_news3_href(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS3_PANEL).get_attribute("href")
+
+    def get_news4_href(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS4_PANEL).get_attribute("href")
+
     def get_news1_panel_title(self):
         return self.driver.find_element(*NewsPageLocators.NEWS1_PANEL_TITLE).text
+
+    def get_news2_panel_title(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS2_PANEL_TITLE).text
+
+    def get_news3_panel_title(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS3_PANEL_TITLE).text
+
+    def get_news4_panel_title(self):
+        return self.driver.find_element(*NewsPageLocators.NEWS4_PANEL_TITLE).text
 
     def get_article_title(self):
         WebDriverWait(self.driver, 10).until(
--- Tests/news_test.py
+++ Tests/news_test.py
@@ -2,7 +2,6 @@ from Tests.base_test import BaseTest
 from Pages.news_page import NewsPageLocators
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
 
 class NewsPageTest(BaseTest):
@@ -10,6 +9,10 @@ class NewsPageTest(BaseTest):
         super().setUp()
         self.news_page = self.home_page.expand_news()
         self.news_page = self.home_page.click_news()
+
+    # def testEnsureNewsIMG(self):
+    #     self.news_page.click_news1()
+    #     self.assertIsNotNone(self.news_page.get_article_img(), "Missing image attribute")
 
     def testVerifyNewsPanel1(self):
         """
@@ -32,6 +35,25 @@ class NewsPageTest(BaseTest):
         # ensure article title is correct
         article_title = self.news_page.get_article_title()
         self.assertEqual(news_title, article_title, "Title mismatch")
-        sleep(2)
 
+    def testVerifyNewsPanel2(self):
+        """
+        Checks Blog 2 navigation menu accuracy
+        """
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(NewsPageLocators.NEWS2_PANEL))
 
+        news2_url = self.news_page.get_news2_href()
+        self.assertIsNotNone(news2_url, "Missing href attribute")
+
+        news_title = self.news_page.get_news2_panel_title()
+
+        # navigate to the URL
+        self.news_page.click_news2()
+
+        # ensure actual navigation is correct
+        self.assertEqual(self.driver.current_url, news2_url, "URL mismatch")
+
+        # ensure article title is correct
+        article_title = self.news_page.get_article_title()
+        self.assertEqual(news_title, article_title, "Title mismatch")
